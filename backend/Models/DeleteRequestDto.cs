5b092727c6cee5ded46537c67028faac9417746d
kerbygarcia <kerbygarcia123@gmail.com>
2026-06-18 08:10:44 +0800

    Delete backend directory

--- backend/Models/DeleteRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace KusinaFlows.Models
-{
-    public class DeleteRequestDto
-    {
-        public string? PerformedBy { get; set; }
-        public string? ApprovedBy { get; set; }
-    }
-}
